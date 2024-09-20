e9e2bc585f6deea130816aebd032bd027eb590ba
Milky <nazarlekomcev77@gmail.com>
2024-09-20 20:22:52 +0300

    1

--- js/config.js
+++ js/config.js
@@ -1,129 +1,51 @@
-/*    _           _    _                     _ 
-     | |         | |  | |                   | |
-  ___| | ___  ___| | _| |     ___   __ _  __| |
- / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
- \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
- |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
-    version 2.05
-*/
+// ignore
+var Config = {};
 
-/*URL for the server logo
-Leave it blank to use text instead*/
-var l_serverImage = "";
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "Area-12 | Monteract Project";
 
-/*Custom server name configuration
-Leave it blank for automatic server name*/
-var l_serverName = "Area-12 | Monteract Project";
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
 
-/*Use a video for the background?
-Otherwise it will just use images*/
-var l_bgVideo = false;
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
 
-/*Video background
-Avaiable options:
-"backgrounds/videos/aurora.webm"
-"backgrounds/videos/bluefall.webm"
-"backgrounds/videos/bluetilefloor.webm"
-"backgrounds/videos/bluewhitecircles.webm"
-"backgrounds/videos/bokehlens.webm"
-"backgrounds/videos/cleanbokeh.webm"
-"backgrounds/videos/coldnight.webm"
-"backgrounds/videos/colorfulburst.webm"
-"backgrounds/videos/dancingstars.webm"
-"backgrounds/videos/dropsflares.webm"
-"backgrounds/videos/focusorangebokeh.webm"
-"backgrounds/videos/glimmering.webm"
-"backgrounds/videos/glowingrings.webm"
-"backgrounds/videos/greencircles.webm"
-"backgrounds/videos/hues.webm"
-"backgrounds/videos/inthedeep.webm"
-"backgrounds/videos/modernflourish.webm"
-"backgrounds/videos/orangeaura.webm"
-"backgrounds/videos/warminside.webm"
-"backgrounds/videos/yellowgreen.webm"
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
 
-You can add custom backgrounds
-Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/glimmering.webm";
-
-/*(Image-background only)
-Place one or more images
-Multiple images will automatically shuffle between them*/
-var l_bgImages = [
-	"backgrounds/images/fond1.jpg",
-	"backgrounds/images/fond2.jpg",
-	"backgrounds/images/fond3.jpg",
-	"backgrounds/images/fond4.jpg",
-	"backgrounds/images/fond5.jpg",
-	"backgrounds/images/fond6.jpg",
-	"backgrounds/images/fond7.jpg",
-	"backgrounds/images/fond8.jpg"
-]
-
-/*Random background images order?*/
-var l_bgImagesRandom = true;
-
-/*(Image-background only)
-Delay between background images changes
-in milliseconds*/
-var l_bgImageDuration = 5000;
-
-/*(Image-background only)
-Image background fade velocity
-in milliseconds*/
-var l_bgImageFadeVelocity = 2000;
-
-/*Enable background overlay?*/
-var l_bgOverlay = true;
-
-/*Background darkening amount
-0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 50;
-
-/*Music player
-'true' or 'false' to enable music in the background*/
-var l_music = true;
-
-/*Display actual song's name?*/
-var l_musicDisplay = true;
-
-/*Music playlist
-Place how much Youtube IDs/.ogg you want*/
-var l_musicPlaylist = [
-    {ogg: "songs/time.ogg", name: "Adventure Time"},
-    {ogg: "songs/byanka.ogg", name: "Мулен Руж"},
-    {ogg: "songs/song.ogg", name: "Little Dark Age"},
-    {ogg: "songs/come.ogg", name: "Come a Little Closer"}
-    //{ogg: "songs/tesla.ogg", name: "Никола Тесла"}
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Добро пожаловать в зону 12!",
+  "Самая быстрая загрузка и лучшая оптимизация только у нас.",
+  "Много новых объектов под ваш выбор",
+  "Уникальная система профессий с уровневой подсистемой"
 ];
 
-/*Random music order?*/
-var l_musicRandom = true;
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
 
-/*Music volume
-Choose a value between 0 and 100*/
-var l_musicVolume = 12;
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
 
-/*Enable custom messages?*/
-var l_messagesEnabled = true;
-
-/*Enter your custom messages below*/
-var l_messages = [
-"| Добро пожаловать в удалённое учреждение Зоны-12 |",
-"| Уникальные системы профессий с уровневой подсистемой |",
-"| Самая быстрая и технологичная загрузка на сервер |",
-"| Уникальные образцы и профессии только у нас! |",
-"| Самый высокий показатель FPS! |",
-"| Приятной игры на Monteract Project! |"
-];
-
-/*Random message order?*/
-var l_messagesRandom = true;
-
-/*Delay between message changes
-in milliseconds*/
-var l_messagesDelay = 5000;
-
-/*Messages fade time
-in milliseconds*/
-var l_messagesFade = 1000;
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
